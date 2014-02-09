704fdc4b0b0fc7f3b0666252d5ba952ed44b3464
cSteusloff <christian.steusloff@googlemail.com>
2014-02-09 23:11:02 +0100

    add minus to SELECT validate

--- lib/queryTranslator.class.php
+++ lib/queryTranslator.class.php
@@ -14,21 +14,27 @@ echo("<pre>");
 $qT = new queryTranslator();
 print_r($qT->translate("
 (
-SELECT CNAME
-FROM COCKTAIL
-)
-MINUS
-(
-SELECT CNAME
-FROM COCKTAIL
+  select 
+    cname 
+  from 
+    cocktail
+) minus (
+  select 
+    cname 
+  from 
+    cocktail natural 
+    join zutat_cocktail
 )
 ", "Jens_"));
 */
 class queryTranslator {
 	
 	//Add $username in front of every tablename of $inputquery
-    public function translate($inputquery, $username){
-        try{
+	
+	public function translate($inputquery, $username){	
+		if (stristr($inputquery, "MINUS")===false)
+		{		
+		try{
 			$parser = new PHPSQLParser();
 			$parsed = $parser->parse($inputquery, TRUE);
 			//print_r($parsed);
@@ -43,8 +49,63 @@ class queryTranslator {
 		catch( Exception $e)
 		{
 			return $e;
+		}		
+		return $inputquery;
 		}
-		return $inputquery;			
+		else
+			return $this->translate_Minus($inputquery, $username);
+	}
+	
+	
+    private function translate_Minus($inputquery, $username){	
+		$first = true;
+		$inputquery = str_ireplace("minus","MINUS",$inputquery);
+        $parts = explode("MINUS",$inputquery);
+		foreach($parts as $part)
+		{			
+		//TODO: remove double white spaces?
+		//Remove superfluous brackets
+		$supBrackets = false;
+		$position1 = strpos($part, "(");
+		if ($position1===0 ||$position1===1 ||$position1===2||$position1===3){
+			$position2 = strrpos($part, ")");
+			if ($position2!=0){		
+				$supBrackets = true;
+				$part = substr_replace($part, "",$position1,1);		
+				$part = substr_replace($part, "",$position2-1,1);
+			}
+		}
+		try{
+			$parser = new PHPSQLParser();
+			$parsed = $parser->parse($part, TRUE);
+			//print_r($parsed);
+			$positions = $this->nameSearch($parsed);
+		
+			$i=0;
+			foreach($positions as $pos){
+				$part = substr_replace($part, $username, $pos+strlen($username)*$i, 0);
+				++$i;
+			}
+		}
+		catch( Exception $e)
+		{
+			return $e;
+		}
+		//Insert removed Brackets
+		if($supBrackets){
+			$part = substr_replace($part, "(",$position1,0);		
+			$part = substr_replace($part, ")",$position2+strlen($username)*$i,0);		
+		}
+		
+		if ($first){		
+			$answer =$part;
+			$first = false;
+			}
+		else
+			$answer .="MINUS".$part;			
+		}
+		
+		return $answer;			
     }
 	//Search for Names in Query
 	private function nameSearch($parsedarray){	
